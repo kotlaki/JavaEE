0f3d6dba4d7bb4e427657dcf22352602a7163622
kotlaki <voland171@mail.ru>
2020-06-23 21:13:05 +0500

    Создали категорию

---
+++ src/main/java/ru/geekbrains/persist/Category.java
@@ -0,0 +1,50 @@
+package ru.geekbrains.persist;
+
+import javax.persistence.*;
+
+@Table(name = "category")
+@Entity
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(length = 4096, nullable = false)
+    private String description;
+
+    public Category() {
+    }
+
+    public Category(Integer id, String name, String description) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
