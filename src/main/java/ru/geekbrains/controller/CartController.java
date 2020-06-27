6b1aa9568d16b6c34e4298a308b48b76ef19da4d
kotlaki <voland171@mail.ru>
2020-06-27 14:02:03 +0500

    Готовим ДЗ

---
+++ src/main/java/ru/geekbrains/controller/CartController.java
@@ -0,0 +1,26 @@
+package ru.geekbrains.controller;
+
+import ru.geekbrains.service.CartService;
+import ru.geekbrains.service.ProductDTO;
+
+import javax.ejb.EJB;
+import javax.enterprise.context.SessionScoped;
+import javax.inject.Named;
+import java.io.Serializable;
+import java.util.List;
+
+@SessionScoped
+@Named
+public class CartController implements Serializable {
+
+    @EJB
+    private CartService cartService;
+
+    public List<ProductDTO> getAllContextCart() {
+        return cartService.getAll();
+    }
+
+    public void add(ProductDTO productDTO) {
+        cartService.add(productDTO);
+    }
+}
---
+++ src/main/java/ru/geekbrains/service/CartService.java
@@ -0,0 +1,20 @@
+package ru.geekbrains.service;
+
+import javax.ejb.Stateful;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+@Stateful
+public class CartService implements Serializable {
+    private List<ProductDTO> cart = new ArrayList<>();
+
+    public List<ProductDTO> getAll() {
+        return cart;
+    }
+
+    public void add(ProductDTO productDTO) {
+        cart.add(productDTO);
+    }
+
+}
